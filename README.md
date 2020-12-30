b0d28bf039ae7198a692c4d09dc529bf619bc69b
herbowicz <herbowicz@gmail.com>
2020-12-30 21:56:37 +0100

    Move styles to index.js

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
     import {calendar} from 'calndar';
 
 ## Options
-    calendar() // displays a calendar for the current year
+    calendar(); // displays a calendar for the current year
 
-    calendar(2025) // displays a calendar for the year 2025
+    calendar(2022); // displays a calendar for the year 2022, default fontFamily is Courier
+
+    calendar(2025, 'Georgia'); // displays a calendar for the year 2025, using Georgia fontFamily
--- index.js
+++ index.js
@@ -1,44 +1,73 @@
-function calendar(year = new Date().getFullYear()) {
+function calendar(
+    year = new Date().getFullYear(),
+    fontFamily = 'Courier',
+) {
 
     const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+    document.body.style.fontFamily = fontFamily;
+
     const title = document.createElement('h1');
     title.innerHTML = year;
+    title.style.textAlign = 'center';
     document.body.appendChild(title);
 
     const container = document.createElement('div');
-    container.classList.add('container');
+    container.style.display = 'grid';
+    container.style.gridTemplateColumns = 'repeat(auto-fit, minmax(320px, 1fr))';
     document.body.appendChild(container);
 
     months.forEach((name, month) => {
         const wrapper = document.createElement('div');
-        wrapper.classList.add('wrapper');
+        wrapper.style.margin = '1em';
         container.appendChild(wrapper);
 
         const title = document.createElement('h2');
-        title.innerHTML = `${name}`;
+        title.innerHTML = name;
+        title.style.textAlign = 'center';
         wrapper.appendChild(title);
 
         const content = document.createElement('ol');
+        content.style.display = 'grid';
+        content.style.gridTemplateColumns = 'repeat(7, 1fr)';
+        content.style.listStyle = 'none';
+        content.style.margin = 0;
+        content.style.padding = 0;
+
         wrapper.appendChild(content);
 
         weekdays.forEach(day => {
             li = document.createElement('li');
-            li.classList.add('day-name');
+            li.style.background = '#eee';
             li.innerHTML = day;
             content.appendChild(li);
         });
 
         for (i = 1; i <= new Date(year, month + 1, 0).getDate(); ++i) {
             li = document.createElement('li');
-            if(i === 1) li.classList.add('first-day');
+            if(i === 1) li.style.gridColumnStart = new Date(year, month, 0).getDay() + 1;
             li.innerHTML = i;
+            li.addEventListener('mouseover', function() {
+                this.style.background = 'lightyellow';
+                this.style.fontWeight = 'bold';
+                this.style.border = '1px solid #333';
+            });
+            li.addEventListener('mouseleave', function() {
+                this.style.background = 'none';
+                this.style.fontWeight = 'normal';
+                this.style.border = '1px solid #eee';
+            });
             content.appendChild(li);
         }
+    });
 
-        const firstDay = document.querySelectorAll('.first-day')[month];
-        firstDay.style.setProperty('--start', new Date(year, month, 0).getDay() + 1);
+    const lis = document.querySelectorAll('li');
+    lis.forEach(li => {
+        li.style.padding = '7px';
+        li.style.textAlign = 'center';
+        li.style.border = '1px solid #eee';
+        li.style.userSelect = 'none';
     });
 }
 
